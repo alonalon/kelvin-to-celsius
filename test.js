883e5021fb43ac32c4578e0a7a69d67b9d956bc8
alonalon <aronhafner@gmail.com>
2016-02-01 00:09:09 +0100

    Init

---
+++ test.js
@@ -0,0 +1,18 @@
+import test from 'ava';
+import k from './';
+
+test(t => {
+	t.is(k(1), -272.15);
+});
+
+test(t => {
+	t.is(k(278), 4.85);
+});
+
+test(t => {
+	t.not(k(20), '-253.15');
+});
+
+test(t => {
+	t.not(k(45), -238.15);
+});
